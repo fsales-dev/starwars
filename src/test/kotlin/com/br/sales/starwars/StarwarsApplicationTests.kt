f758ab773c9bf3bc6af3be61fc0ae50b6cee170e
Felipe <felipe.sales.dev@gmail.com>
2021-05-27 07:32:22 -0300

    first commit

---
+++ src/test/kotlin/com/br/sales/starwars/StarwarsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.sales.starwars
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarwarsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
