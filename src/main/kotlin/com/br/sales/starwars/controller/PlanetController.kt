ccb1946573da1c68f725d5c5368199fe996c9263
fsales-dev <felipe.sales.dev@gmail.com>
2021-05-28 07:21:24 -0300

    Criação dos metodos de crud na controller

--- src/main/kotlin/com/br/sales/starwars/controller/PlanetController.kt
+++ src/main/kotlin/com/br/sales/starwars/controller/PlanetController.kt
@@ -1,10 +1,47 @@
 package com.br.sales.starwars.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.br.sales.starwars.model.Planet
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/planets")
 class PlanetController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+
+        return "Cheguei"
+    }
+
+    @PostMapping()
+    fun create(@RequestBody planet: Planet): ResponseEntity<Planet> {
+
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/{id}")
+    fun findPlanetById(@PathVariable id: Long): ResponseEntity<Planet> {
+
+        return ResponseEntity.ok().build()
+    }
+
+    @PutMapping("/{id}")
+    fun update(@RequestBody planet: Planet, @PathVariable("id") id: Long): ResponseEntity<Planet> {
+
+        return ResponseEntity.ok().build()
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable("id") id: Long): ResponseEntity<Unit> {
+
+        return ResponseEntity.noContent().build()
+    }
+
+    @GetMapping()
+    fun findAll(): ResponseEntity<List<Planet>> {
+
+        return ResponseEntity.ok().build()
+    }
 
 }
